bb35d9e7c3b3519963fe82adf2dbbb9a0a8f2aac
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-03 16:01:39 +0200

    prepare 1.21.7 port

---
+++ 1.21.7/NeoForge/src/main/java/fuzs/resourcepackoverrides/neoforge/ResourcePackOverridesNeoForge.java
@@ -0,0 +1,13 @@
+package fuzs.resourcepackoverrides.neoforge;
+
+import fuzs.resourcepackoverrides.ResourcePackOverrides;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ResourcePackOverrides.MOD_ID)
+public class ResourcePackOverridesNeoForge {
+
+    public ResourcePackOverridesNeoForge() {
+        // NO-OP
+    }
+}
